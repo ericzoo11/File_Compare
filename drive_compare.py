242b7d1c5e238e7fe5cc7a3b92e8d7acb3fe8ecf
ericzhu <zhueric27@gmail.com>
2021-04-17 17:24:21 -0400

    Script done

--- drive_compare.py
+++ drive_compare.py
@@ -1,7 +1,66 @@
 import os 
 import filecmp as fc
 import sys 
+import re
 
+def parse_report():
+    re = [] #list for report
+    lines = []# list for lines with "Only in"
+
+    f = open('output_file.txt', 'r')
+    rep = f.readlines()
+    f.close()
+    
+    #add only lines that have differences
+    for line in rep:
+        if "Only in" in line:
+            lines.append(line)
+
+    return lines
+
+def org_list(listA):
+    main_list = {}
+    
+    for item in listA:
+        
+        temp = item.split()
+        a = temp[2] #holds the path of the files
+        b = re.findall(r"'(.*?)'", item, re.DOTALL) #list of diff files
+        
+        #add path as key and diff files as values in dictionary
+        main_list[a] = b
+
+    return main_list
+
+######
+#Main
+######
+
+if __name__ == "__main__":
+
+    root_dir1 = '/Users/ericzhu/Documents/My_Stuff/Resume_Stuff/'
+    root_dir2 = '/Users/ericzhu/Desktop/Resume_Stuff/'
+
+    dog = fc.dircmp(root_dir1, root_dir2, ignore=None, hide=None)
+    
+    default_stdout = sys.stdout #saving current stdout to var
+    file_handle = open('output_file.txt', 'w')
+
+    #output report to file 
+    try:
+        sys.stdout = file_handle
+        dog.report_full_closure() 
+        sys.stdout.close()
+    finally:
+        #revert to default stdout
+        sys.stdout = default_stdout
+    print("\n\n\n")
+    
+    # data is now in dictionary ready to go to extract and print
+    data_ready = org_list(parse_report()) 
+    
+
+''' First go at script functions
 def dir_diff(dir_obj):
     
     # grab left and right directories with the help of the built in function 
@@ -49,24 +108,4 @@ def dir_iterate(dir1, dir2):
     for subdir, dirs, files in os.walk(dir1):
         print(subdir)
         for file in files:
-            pass
-######
-#Main
-######
-
-if __name__ == "__main__":
-
-    root_dir1 = '/Users/ericzhu/Documents/My_Stuff/Resume_Stuff/'
-    root_dir2 = '/Users/ericzhu/Desktop/Resume_Stuff/'
-    #dog = fc.dircmp(root_dir1, root_dir2)
-    #dir_diff(dog)
-    #dir_iterate(root_dir1, root_dir2)
-    dog = fc.dircmp(root_dir1, root_dir2, ignore=None, hide=None)
-    
-    sys.stdout = open('output_file.txt', 'w')
-    dog.report_full_closure() 
-    sys.stdout.close()
-
-    #f = open("output_file", "w")
-    #f.write(cat)
-    #f.close
+            pass'''
